8b8b2bcbbc3f14f7a4f0ccb8b6cda1f3988b4b28
tujianglin <jianglin.tu@wimisoft.com>
2024-05-21 23:16:55 +0800

    feat: 1

--- build/vite/index.ts
+++ build/vite/index.ts
@@ -1,10 +1,9 @@
 import { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueSetupExtend from 'vite-plugin-vue-setup-extend';
 import Unocss from 'unocss/vite';
-export function createVitePlugins () {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx(), vueSetupExtend(), Unocss()];
+export function createVitePlugins() {
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx(), Unocss()];
 
   return vitePlugins;
 }
